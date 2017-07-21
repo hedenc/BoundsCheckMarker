8a1ff7628c4fd4b267e8093c18606e521c1be4ea
hedenc <cryofreezed@yahoo.com>
2017-07-21 14:49:55 +0900

    Fixed bug in code block search

--- analyser.cpp
+++ analyser.cpp
@@ -24,6 +24,18 @@ void analyse(FILE *file, ast &tree)
 
     size_t num_checks = 0;
 
+    /*
+    for (auto &cb: blocks) {
+        printf("%llx <- ", cb.startinsno());
+        for (auto &b: cb.backtrace())
+            printf("%llx ", b);
+        printf("-> ");
+        for (auto &f: cb.fwdptrs())
+            printf("%llx ", f);
+        printf("\n");
+    }
+    */
+
     for (auto &cb: blocks) {
         size_t chckkind;
         auto pos = cb.source().find("softboundcets.h");
@@ -41,24 +53,33 @@ void analyse(FILE *file, ast &tree)
             }
             stack<code_block *> dfs;
             set<uint64_t> visited;
+            //printf("track %llx", cb.startinsno());
             for (auto &bp: cb.backtrace()) {
+                //printf("try push %llx\n", bp);
                 dfs.push(find_block(blocks, bp));
+                //printf("push %llx\n", find_block(blocks, bp)->startinsno());
                 visited.insert(bp);
             }
             while (!dfs.empty()) {
                 code_block *src = dfs.top();
+                //printf("pop %llx\n", src->startinsno());
                 dfs.pop();
-                if (src->source().find("softbound") == string::npos)
+                if (src->source().find("softbound") == string::npos) {
+                    //printf(" found %llx", src->startinsno());
                     tree[src->source()][src->lineno()][chckkind].insert(
                         src->startinsno()
                     );
+                }
                 else 
                     for (auto bbp: src->backtrace())
                         if (!visited.count(bbp)) {
+                            //printf("try push %llx\n", bbp);
                             dfs.push(find_block(blocks, bbp));
+                            //printf("push %llx\n", find_block(blocks, bbp)->startinsno());
                             visited.insert(bbp);
                         }
             }
+            //printf("\n");
         }
     }
     //printf("Number of check code blocks in binary: %zu\n", num_checks);
--- code_block.cpp
+++ code_block.cpp
@@ -10,18 +10,15 @@ void code_block::doublelink(code_block &to, uint64_t split_addr)
 
 void code_block::handle_split(uint64_t split_addr, code_block &from, bool last_block)
 {
-    //fprintf(stderr, "handle_split(%lx, %lx)\n", split_addr, from.startinsno_);
     startinsno_ = split_addr;
     this[-1].fwdptrs_ = {startinsno_};
     backtrace_ = {this[-1].startinsno_, from.startinsno_};
     from.fwdptrs_.insert(split_addr);
 
-    //fprintf(stderr, "debug %p\n", (void *) dbg);
     if (!last_block) {
         auto search = this[1].backtrace_.find(this[-1].startinsno_);
 
         if (search != this[1].backtrace_.end()) {
-            //fprintf(stderr, "if\n");
             this[1].backtrace_.erase(search);
             this[1].backtrace_.insert(
                 this[-1].startinsno_
@@ -34,10 +31,9 @@ code_block *find_block(uint64_t insno, code_block *beg, size_t sz)
 {
     if (sz == 1)
         return beg;
-    if (insno < beg[sz/2 +1].startinsno())
-        return find_block(insno, beg, sz/2);
-    else
+    if (insno >= beg[sz/2].startinsno())
         return find_block(insno, beg + sz/2, sz - sz/2);
+    return find_block(insno, beg, sz/2);
 }
 
 code_block *find_block(vector<code_block> &blocks, uint64_t insno)
